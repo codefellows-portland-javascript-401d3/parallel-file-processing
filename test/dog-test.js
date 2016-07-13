0ec403b04867951bb580c627c411e4cae1a79973
Danielle H <daheber@gmail.com>
2016-07-13 13:29:41 -0700

    data for dogs added and base outline for the test

---
+++ test/dog-test.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+
+describe('get dogs', ()=>{
+  it('gets all dogs', done=>{
+
+  });
+});
+
+function getDogs(directory, callback){
+  //create array to store returned dogs
+  //look in dog folder
+  //grab each json file
+  //read the file
+  //push each dog into the array
+  //return the list of dogs
+}
