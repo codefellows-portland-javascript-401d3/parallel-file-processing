1b27761600e37607088ab10ec63de333c6050614
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-07-13 15:25:18 -0700

    reading now works

--- test/create.test.js
+++ test/create.test.js
@@ -1,29 +1,43 @@
 const assert = require('assert');
 const create = require('../lib/create');
+const read = require('../lib/read');
 const fs = require('fs');
 
-describe('Create', function() {
+const testData = '12345';
+
+describe('Create and Read', function() {
 
   before(function() {
-      fs.mkdirSync('.test_files');
+      fs.mkdirSync('.test_files/');
     });
 
-  it('loads the library', function() {
+  it('loads the create library', function() {
     assert.ok(create);
   });
 
+  it('loads the read library', function() {
+    assert.ok(read);
+  });
+
   it('creates a file', function(done) {
-    create('.test_files/test-file', function() {
+    create('.test_files/test-file', testData, function() {
       done();
     });
   });
 
-  // it('correctly stores data', function() {
-
-  // })
+  it('correctly stores data', function(done) {
+    read('.test_files/test-file', function(err,data) {
+      if(err) throw new Error(err);
+      else {
+        if (data != testData) throw new Error('Does not compute.');
+        done();
+      }
+    })
+  })
 
   after(function() {
       fs.unlinkSync('.test_files/test-file');
       fs.rmdirSync('.test_files/');
     });
 });
+
