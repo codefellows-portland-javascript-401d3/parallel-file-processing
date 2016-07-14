6fa50980df0798ba524bce9b6ac4ee75637dc7c2
Danielle H <daheber@gmail.com>
2016-07-14 15:40:03 -0700

    file structure organization

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 A file store that does the following:
 
-1. Retrieve all objects of a given type (getAllObjects.js)
-2. Retrieve specific items and return them in an array in the order they were called (getItem.js)
+1. Retrieve all objects of a given type
+2. Retrieve specific items and return them in an array in the order they were called
--- test/dog-test.js
+++ test/dog-test.js
@@ -1,28 +1,32 @@
 const assert = require('assert');
-const getAllObjects = require('../getAllObjects');
+const getAllObjects = require('../lib/getAllObjects');
+const getItem = require('../lib/getItem');
 
 describe('get dogs', ()=>{
 
-  var dogContents = [ { name: 'dog 1', id: '1', type: 'dog' },
+  var allDogs = [ { name: 'dog 1', id: '1', type: 'dog' },
   { name: 'dog2', id: '2', type: 'dog' },
   { name: 'dog3', id: '3', type: 'dog' },
   {name : 'dog 4', id : '4', type : 'dog'}];
 
+  var someDogs = [ { name: 'dog2', id: '2', type: 'dog' },
+  { name: 'dog 1', id: '1', type: 'dog' },
+  { name: 'dog3', id: '3', type: 'dog' } ];
+
   it('gets all dogs', done=>{
     getAllObjects( __dirname + '/../dog', (error, fileContents)=>{
-      if (error) return done(error);
-      assert.deepEqual(fileContents, dogContents);
+      if(error) return done(error);
+      assert.deepEqual(fileContents, allDogs);
       done();
     });
-
   });
 
-  // it('retrieves select dogs in the same order', done=>{
-  //
-  // });
-//
-//   it('stores a dog'. ()=>{
-//
-//   });
-//
+  it('retrieves select dogs in the same order', done=>{
+    getItem('"dog2", "dog1", "dog3"', (error, fileContents)=>{
+      if(error) return done(error);
+      assert.deepEqual(fileContents, someDogs);
+      done();
+    });
+  });
+
 });
