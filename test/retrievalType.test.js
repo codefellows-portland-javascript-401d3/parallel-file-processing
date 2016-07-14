12ed339d259ae7c9bf74322aac5f0e97b2df024c
Arielle Foldoe <afoldoe@gmail.com>
2016-07-14 10:04:08 -0700

    updated tests

--- test/retrievalType.test.js
+++ test/retrievalType.test.js
@@ -7,6 +7,7 @@ const retrievalType = require('../lib/retrievalType');
 describe('it retrieves the data based on the type', () => {
 	it('has type bike', (done) => {
 		retrievalType(__dirname + '/../data/', 'bike', (err, selected) => {
+			if (err) {return done(err);};
 			assert.deepEqual(selected, 
 			[{ "id": "1", "type": "bike", "make": "Schwinn", "model": "Varsity"},
 			{"id": "2", "type": "bike", "make": "Pake", "model": "RumRunner"},
